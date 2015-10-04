2b6e292488089919f674dd25a7abaa272de5503d
Elemnir <ahoward0920@gmail.com>
2015-10-04 19:17:26 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,75 @@
+import socket
+import threading
+
+class ChatRoom(object):
+    def __init__(self):
+        self.cv = threading.Condition()
+        self.pending = []
+        self.clients = {}
+        threading.Thread(target=self.run_room).start()
+        
+    def run_room(self):
+        self.cv.acquire()
+        while 1:
+            self.cv.wait()
+            for name in self.clients.keys():
+                try:
+                    for mesg in self.pending:
+                        self.clients[name].sendall(mesg)
+                except:
+                    del self.clients[name]
+            self.pending[:] = []
+
+    def add_client(self, name, client):
+        self.cv.acquire()
+        self.clients[name] = client
+        client.sendall('Members: {}\n'.format(', '.join(self.clients.keys())))
+        self.pending.append(name + ' has joined\n')
+        self.cv.notify()
+        self.cv.release()
+    
+    def enqueue_message(self, mesg):
+        self.cv.acquire()
+        self.pending.append(mesg)
+        self.cv.notify()
+        self.cv.release()
+
+
+class ChatServer(object):
+    def __init__(self, host, port):
+        self.rooms = {}
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host,port))
+        s.listen(1)
+        while 1:
+            conn, addr = s.accept()
+            t = threading.Thread(target=self.serve_client, args=(conn,))
+            t.daemon = True
+            t.start()
+    
+    def serve_client(self, conn):
+        try:
+            conn.sendall("Enter your name: ")
+            cname = conn.recv(1024).rstrip()
+            conn.sendall("Rooms: {}\n".format(', '.join(self.rooms.keys())))
+            conn.sendall("Name a Room to join/create: ")
+            roomname = conn.recv(1024).rstrip()
+
+            if roomname not in self.rooms.keys():
+                self.rooms[roomname] = ChatRoom()
+            room = self.rooms[roomname]
+            room.add_client(cname, conn)
+        except:
+            return
+
+        while 1:
+            mesg = conn.recv(4096)
+            if not mesg:  
+                room.enqueue_message(cname + ' has left\n')
+                break
+            room.enqueue_message(cname + ': ' + mesg.rstrip() + '\n')
+
+
+if __name__ == "__main__":
+    print 'Starting Server...'
+    ChatServer('127.0.0.1', 8060)
